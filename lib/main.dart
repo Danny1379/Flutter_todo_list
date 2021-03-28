b0c28a309a6ef1a9aa1fc94fd811a1ba06452064
Danny <dazza1379@gmail.com>
2021-03-28 16:22:37 +0430

    Added Border to list tiles placing them in Container , Added Saving data to sharedPrefrences to be read on app restart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 // Import MaterialApp and other widgets which we can use to quickly create a material app
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(new TodoApp());
 
@@ -21,7 +24,12 @@ class TodoList extends StatefulWidget {
 class TodoListState extends State<TodoList> {
   List<String> _listItems = [] ;
 
-
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _readData();
+  }
   void _addNewItem(String task){
     if(task.length > 0){
       setState(() => _listItems.add(task));
@@ -31,6 +39,7 @@ class TodoListState extends State<TodoList> {
 
   void _removeTodoItem(int index){
     setState(() => _listItems.removeAt(index)) ;
+    _saveData();
   }
 
 
@@ -41,11 +50,11 @@ class TodoListState extends State<TodoList> {
         return new AlertDialog(
           title : new Text("Do you want to remove '${_listItems[index]}'?"),
           actions: <Widget>[
-            new FlatButton(
+            new TextButton(
               child: new Text('No'),
               onPressed: () => Navigator.of(context).pop(),
             ),
-            new FlatButton(
+            new TextButton(
               child: new Text('Yes'),
               onPressed: () {
                 _removeTodoItem(index);
@@ -62,12 +71,23 @@ class TodoListState extends State<TodoList> {
       // ignore: missing_return
       itemBuilder: (context , index){
         if(index < _listItems.length){
-          return _buildTodoItem(_listItems[index],index);
+          return _buildItem(_listItems[index],index);
         }
       },
     );
   }
+  _saveData() async{
+    String jsonString = jsonEncode(_listItems);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('jsonList', jsonString);
+  }
 
+  _readData() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String jsonString = prefs.getString('jsonList');
+    List<dynamic> jsonList = jsonDecode(jsonString) ;
+    _listItems = jsonList.map((n) => n.toString()).toList();
+  }
   void _pushAddTodoScreen(){
     Navigator.of(context).push(
       new MaterialPageRoute(
@@ -81,6 +101,7 @@ class TodoListState extends State<TodoList> {
               autocorrect: true,
               onSubmitted: (task){
                 _addNewItem(task) ;
+                _saveData();
                 Navigator.of(context).pop();
               },
               decoration: new InputDecoration(
@@ -96,15 +117,16 @@ class TodoListState extends State<TodoList> {
   Widget _buildTodoItem(String todoText,int index){
     return new ListTile(
       title: new Text(todoText),
-      onTap: () => _promptRemoveTodoItem(index)
+      onTap: () => _promptRemoveTodoItem(index),
+      tileColor: Color(0x42583D1C),
     );
   }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: Color(0xFFF6EDDC),
+      backgroundColor: Color(0xFFFFFFFF),
         appBar: new AppBar(
-          backgroundColor:  Color(0xFF745E4D),
+          backgroundColor:  Color(0xFF65422F),
             title: new Text('Todo List')
         ),
       body: _buildTodoList(),
@@ -112,6 +134,15 @@ class TodoListState extends State<TodoList> {
         onPressed: _pushAddTodoScreen,
         tooltip: "new task" ,
         child: new Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget _buildItem(String todoText,int index,) {
+    return new Container(
+      child: _buildTodoItem(todoText, index),
+      decoration: new BoxDecoration(
+        border: Border.all(width: 1.0,color: Color(0x000000))
       ),
     );
   }
